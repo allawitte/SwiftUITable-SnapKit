6f741d6288c4044fc49a4572ab8be37aa5ac658a
Belotta <bellotta@gmail.com>
2024-05-05 14:18:04 +0300

    Cells configuration with Model

---
+++ M15Pract/Model/Comment.swift
@@ -0,0 +1,8 @@
+//
+//  Comment.swift
+//  M15Pract
+//
+//  Created by Alla on 05/05/2024.
+//
+
+import Foundation
---
+++ M15Pract/Model/CommentsStorage.swift
@@ -0,0 +1,8 @@
+//
+//  CommentsStorage.swift
+//  M15Pract
+//
+//  Created by Alla on 05/05/2024.
+//
+
+import Foundation

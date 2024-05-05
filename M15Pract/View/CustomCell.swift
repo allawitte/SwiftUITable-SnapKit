4c86b0ea612e60e7538107f7a9f8e43bcf78f174
Belotta <bellotta@gmail.com>
2024-05-05 13:03:51 +0300

    Clean markup

---
+++ M15Pract/View/CustomCell.swift
@@ -0,0 +1,13 @@
+//
+//  TableViewCell.swift
+//  M15Pract
+//
+//  Created by Alla on 03/05/2024.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    static let identifyer = "customCell"
+}
